fefb679352a3b641e2dcd8688f8d96c02ce74017
LaurenGilchrist <laurengilchrist@hotmail.com>
2018-06-05 16:39:17 -0400

    not recognizing odie line 13 of app

--- src/main/java/shelterAmok/VirtualPetAmokApp.java
+++ src/main/java/shelterAmok/VirtualPetAmokApp.java
@@ -3,15 +3,18 @@ package shelterAmok;
 import java.util.Scanner;
 
 public class VirtualPetAmokApp {
-	
+
 	Scanner input = new Scanner(System.in);
 	
 	Shelter shelter = new Shelter();
 	
-	clifford = new Dog("Clifford", 10, 10, 10, 10, 10, 10, "loves to play catch");
-	garfield = new Cat("Garfield", 10, 10, 10, 10, 10, 10, "loves to nap and cuddle");
-	odie = new RoboDog("Odie", 10, 10, 10, 10, "loves to wag his tail");
-	sylvester = new RoboCat("Sylvester", 10, 10, 10, 10, "loves to purr");
+	Dog clifford = new Dog("Clifford", 10, 10, 10, 10, 10, 10, "loves to play catch");
+	Cat garfield = new Cat("Garfield", 10, 10, 10, 10, 10, 10, "loves to nap and cuddle");
+	RoboDog odie = new  RoboDog("Odie", 10, 10, 10, 10, "loves to wag his tail");
+	RoboCat sylvester = new RoboCat("Sylvester", 10, 10, 10, 10, "loves to purr");
+	
+	
+	
 	
 }
 	
